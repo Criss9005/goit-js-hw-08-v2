753e9cc9f93418acc8771369fe81db2dbeb769b7
Criss9005 <cdjpn5@gmail.com>
2023-11-19 20:21:51 -0500

    update 1.1

--- src/index.js
+++ src/index.js
@@ -1,51 +1,47 @@
 import axios from "axios";
 import SlimSelect from 'slim-select'
+import Notiflix from 'notiflix';
 
 axios.defaults.headers.common["x-api-key"] = "live_cm4VvcL13mJC0Z6PXYsVqPJ4kugv341iWtwgFhjXECmiBHMq4nTNEzrGgpELYQ3V";
-const select1 = document.querySelector('.breed-select')
+let selectList = document.querySelector('.breed-select')
 const container = document.querySelector('.cat-info')
+const loader = document.querySelector('.loader')
+const fail = document.querySelector('.error')
+
 export let names = {}
 
-new SlimSelect({
-  select: '.breed-select'
-})
-
+fetchBreeds()
 
 function fetchBreeds() { 
-    
 axios.get('https://api.thecatapi.com/v1/breeds')
-  .then(function (response) {
+    .then(function (response) {
+      
       // manejar respuesta exitosa
-      console.log(response.data)
       names = response.data.map(item => ({ name: item.name, id: item.id, description: item.description, temperament: item.temperament }))
-      //console.log(names);
       names.forEach(element => {
-      select1.innerHTML +=`<option value=${element.id}>${element.name}</option>`
-    
+      selectList.innerHTML +=`<option value=${element.id}>${element.name}</option>`
       }); 
-     
+          new SlimSelect({
+          select: '.breed-select'
+          })
+        loader.style.display = 'none' 
+        fail.style.display = 'none' 
   })
   .catch(function (error) {
-    // manejar error
-      console.log(error);
-    console.log('error');
-      
+      // manejar error
+      Notiflix.Notify.failure('Fallo al procesar la Solicitud, Por favor verificar API');
+      loader.style.display = 'flex'
+      fail.style.display = 'flex'    
   })
-  .finally(function () {
-    // siempre sera executado
-  });
-
 }
 
-fetchBreeds()
+
 
 function fetchCatByBreed(breedId) { 
 
     axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
   .then(function (response) {
       // manejar respuesta exitosa
-      console.log(response)
-      console.log(response.data[0].url)
       names.forEach( cat => {
      if (cat.id == breedId) {
          container.innerHTML = ` <img src=${response.data[0].url} alt="${cat.name} cat photo" width="300px" heigth="300px">
@@ -61,18 +57,13 @@ function fetchCatByBreed(breedId) {
   })
   .catch(function (error) {
     // manejar error
-      console.log(error);
-    console.log('error');
+      Notiflix.Notify.failure('Fallo al procesar la Solicitud, Por favor verificar Raza');
       
   })
-  .finally(function () {
-    // siempre sera executado
-  });
-
 } 
 
 
-select1.addEventListener('change', (e) => { 
+selectList.addEventListener('change', (e) => { 
     //e.target.value aca tengo el id
     fetchCatByBreed(e.target.value)
         
